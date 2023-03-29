05ee3440be37e9a204860a8e840573f64a94f890
Mingyu Li <mingyu.li@tier4.jp>
2023-03-29 19:35:35 +0900

    use synchronized roi and signal in multi camera fusion

--- perception/traffic_light_multi_camera_fusion/include/traffic_light_multi_camera_fusion/node.hpp
+++ perception/traffic_light_multi_camera_fusion/include/traffic_light_multi_camera_fusion/node.hpp
@@ -40,21 +40,43 @@
 #include <geometry_msgs/msg/pose_stamped.hpp>
 #include <sensor_msgs/msg/camera_info.hpp>
 
+#include <message_filters/subscriber.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <message_filters/sync_policies/exact_time.h>
+#include <message_filters/synchronizer.h>
+
 #include <memory>
 #include <set>
 #include <vector>
 
 namespace traffic_light
 {
+
+namespace mf = message_filters;
+
 class MultiCameraFusion : public rclcpp::Node
 {
+  typedef autoware_auto_perception_msgs::msg::TrafficSignalArray SignalType;
+  typedef autoware_auto_perception_msgs::msg::TrafficLightRoiArray RoiType;
+
 public:
   explicit MultiCameraFusion(const rclcpp::NodeOptions & node_options);
 
 private:
-  std::vector<
-    rclcpp::Subscription<autoware_auto_perception_msgs::msg::TrafficSignalArray>::SharedPtr>
-    signal_subs_;
+  void trafficSignalRoiCallback(
+    const RoiType::ConstSharedPtr roi_msg, const SignalType::ConstSharedPtr signal_msg);
+
+  typedef mf::sync_policies::ExactTime<RoiType, SignalType> ExactSyncPolicy;
+  typedef mf::Synchronizer<ExactSyncPolicy> ExactSync;
+
+  typedef mf::sync_policies::ApproximateTime<RoiType, SignalType> ApproSyncPolicy;
+  typedef mf::Synchronizer<ApproSyncPolicy> ApproSync;
+
+  std::vector<std::unique_ptr<mf::Subscriber<SignalType>>> signal_subs_;
+  std::vector<std::unique_ptr<mf::Subscriber<RoiType>>> roi_subs_;
+  std::vector<std::unique_ptr<ExactSync>> exact_sync_subs_;
+  std::vector<std::unique_ptr<ApproSync>> appro_sync_subs_;
+  bool is_approxiate_sync_;
 };
 }  // namespace traffic_light
 #endif  // TRAFFIC_LIGHT_MULTI_CAMERA_FUSION__NODE_HPP_
--- perception/traffic_light_multi_camera_fusion/src/node.cpp
+++ perception/traffic_light_multi_camera_fusion/src/node.cpp
@@ -1,16 +1,3 @@
-// Copyright 2020 Tier IV, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 /*
  * Copyright 2023 Autoware Foundation. All rights reserved.
  *
@@ -43,12 +30,42 @@ MultiCameraFusion::MultiCameraFusion(const rclcpp::NodeOptions & node_options)
 : Node("traffic_light_multi_camera_fusion", node_options)
 {
   using std::placeholders::_1;
+  using std::placeholders::_2;
+
+  is_approxiate_sync_ = declare_parameter("approximate_sync", false);
   std::vector<std::string> camera_namespaces =
-    this->declare_parameter("input/camera_namespaces", std::vector<std::string>{});
-  std::cout << "ns size = " << camera_namespaces.size() << std::endl;
-  for (auto n : camera_namespaces) {
-    std::cout << n << std::endl;
+    this->declare_parameter("camera_namespaces", std::vector<std::string>{});
+  for (const std::string & camera_ns : camera_namespaces) {
+    std::string signal_topic = camera_ns + "/traffic_signals";
+    std::string roi_topic = camera_ns + "/rois";
+    roi_subs_.emplace_back(
+      new mf::Subscriber<RoiType>(this, roi_topic, rclcpp::QoS{1}.get_rmw_qos_profile()));
+    signal_subs_.emplace_back(
+      new mf::Subscriber<SignalType>(this, signal_topic, rclcpp::QoS{1}.get_rmw_qos_profile()));
+
+    if (is_approxiate_sync_) {
+      appro_sync_subs_.emplace_back(
+        new ApproSync(ApproSyncPolicy(10), *(roi_subs_.back()), *(signal_subs_.back())));
+      appro_sync_subs_.back()->registerCallback(
+        std::bind(&MultiCameraFusion::trafficSignalRoiCallback, this, _1, _2));
+    } else {
+      exact_sync_subs_.emplace_back(
+        new ExactSync(ExactSyncPolicy(10), *(roi_subs_.back()), *(signal_subs_.back())));
+      exact_sync_subs_.back()->registerCallback(
+        std::bind(&MultiCameraFusion::trafficSignalRoiCallback, this, _1, _2));
+    }
   }
+}
+
+void MultiCameraFusion::trafficSignalRoiCallback(
+  const RoiType::ConstSharedPtr roi_msg, const SignalType::ConstSharedPtr signal_msg)
+{
+  RCLCPP_INFO_STREAM(
+    get_logger(), "roi header = " << roi_msg->header.frame_id
+                                  << ", signal header = " << signal_msg->header.frame_id);
+  double stampDiff = rclcpp::Time(roi_msg->header.stamp).seconds() -
+                     rclcpp::Time(signal_msg->header.stamp).seconds();
+  RCLCPP_INFO_STREAM(get_logger(), "stamp diff = " << stampDiff);
 }
 
 }  // namespace traffic_light
